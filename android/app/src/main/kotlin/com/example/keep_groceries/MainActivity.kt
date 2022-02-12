d58ca91e1956d31a8847d1b4bd35e319dae8bcc0
Yeasin <realalyeasin@gmail.com>
2022-02-12 21:09:29 +0600

    keep_groceries

---
+++ android/app/src/main/kotlin/com/example/keep_groceries/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.keep_groceries
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'FirebaseCRUD.dart';
+
+void main() async {
+  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      home: HomePage(),
+    );
+  }
+}
